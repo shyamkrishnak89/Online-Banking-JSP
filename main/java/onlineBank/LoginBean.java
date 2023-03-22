641a4d121befd867898d473b64fa7c2a4e63c68a
Shyam Krishna K <shyamkrishnak89@gmail.com>
2023-03-22 15:58:03 +0530

    Update LoginBean.java

--- main/java/onlineBank/LoginBean.java
+++ main/java/onlineBank/LoginBean.java
@@ -1,25 +1,24 @@
 package onlineBank;
 
-public class LoginBean {  
-private String username,pass;
-
-public String getUsername() {
-	return username;
+public class LoginBean {
+	private String username,pass;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public int getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	private int accountNo;
 }
-
-public void setUsername(String username) {
-	this.username = username;
-}
-
-public String getPass() {
-	return pass;
-}
-
-public void setPass(String pass) {
-	this.pass = pass;
-}  
-  
-
-  
-  
-}  
