393dc422c2fbb2e9350d935f5d0d673ff23ac5e9
Shyam Krishna K <shyamkrishnak89@gmail.com>
2023-03-22 15:58:32 +0530

    Update LoginDao.java

--- main/java/onlineBank/LoginDao.java
+++ main/java/onlineBank/LoginDao.java
@@ -1,29 +1,30 @@
 package onlineBank;
+import java.sql.*;
 
-import java.sql.*;  
-public class LoginDao {  
-  
-public static boolean validate(LoginBean bean){  
-boolean status=false;  
-try{  
-Connection con=GetCon.getCon();  
-String userName = bean.getUsername();
-String passWord = bean.getPass();
-System.out.println(userName);
-System.out.println(passWord);
-              
-PreparedStatement ps=con.prepareStatement(  
-    "select * from NEWACCOUNT where username=? and password=?");  
-  
-ps.setString(1,userName);  
-ps.setString(2, passWord);  
-              
-ResultSet rs=ps.executeQuery();  
-status=rs.next();  
-              
-}catch(Exception e){}  
-  
-return status;  
-  
-}  
-}  
+public class LoginDao {
+	public static boolean validate(LoginBean bean){  
+		boolean status=false;  
+		try{  
+		Connection con=GetCon.getCon();  
+		String userName = bean.getUsername();
+		int accountNum = bean.getAccountNo();
+		String passWord = bean.getPass();
+		System.out.println("From Dao"+userName);
+		System.out.println("From Dao"+passWord);
+		              
+		PreparedStatement ps=con.prepareStatement(  
+		    "select * from NEWACCOUNT where accountno=? and password=?");  
+		  
+		ps.setInt(1,accountNum);  
+		ps.setString(2, passWord);  
+		              
+		ResultSet rs=ps.executeQuery();  
+		status=rs.next();  
+		              
+		}catch(Exception e){}  
+		  
+		return status;  
+		  
+		}  
+
+}
